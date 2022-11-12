6b680a2abeaf4615d405d1a0b6c7a3e5e3aabc0c
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-11-12 22:13:48 +0200

    Add repository, dependency injection, change favicon, core, infrastructure e.t.c

---
+++ TheScene.Infrastructure/Migrations/20221112192432_start.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheScene.Infrastructure.Migrations
+{
+    public partial class start : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ TheScene/Extensions/TheSceneCollectionService.cs
@@ -0,0 +1,14 @@
+﻿using TheScene.Infrastructure.Data.Common;
+
+namespace TheScene.Extensions.DependencyInjection
+{
+    public static class TheSceneCollectionService
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository, Repository>();
+
+            return services;
+        }
+    }
+}
