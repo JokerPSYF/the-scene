0f641a53f5ddc6aa1cdb00097281d4fd015d22af
Todor Vasilev <todorvasilev005@gmail.com>
2022-11-22 12:00:32 +0200

    edit method all in eventService, implemented eventController

--- TheScene/Controllers/EventController.cs
+++ TheScene/Controllers/EventController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TheScene.Core.Interface;
 using TheScene.Core.Models.Event;
 
 namespace TheScene.Controllers
 {
     public class EventController : Controller
     {
+        private readonly IEventService eventService;
+
+        public EventController(IEventService _eventService)
+        {
+            this.eventService = _eventService;
+        }
+
         public async Task<IActionResult> Details()
         {
             var model = new DetailEventModel();
@@ -12,9 +20,11 @@ namespace TheScene.Controllers
             return View(model);
         }
 
-        public async Task<IActionResult> All()
+        public async Task<IActionResult> All(string? genre = null, string? perfomanceType = null,
+            string? searchTerm = null, EventSorting sorting = EventSorting.Newest,
+            int currentPage = 1, int eventPerPage = 5)
         {
-            var model = new EventQueryModel();
+            var model = eventService.All(genre, perfomanceType, searchTerm, sorting, currentPage, eventPerPage);
 
             return View(model);
         }
